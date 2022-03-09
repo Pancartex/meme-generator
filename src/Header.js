9aba6236ef49915212579e09928f7de18ebd2f8c
Jonathan <jonathan.gb001@gmail.com>
2022-03-09 00:17:32 -0500

    more customization

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,12 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <div className="meme-img-container">
-        <img src="src/images/troll-face.png" className="header--image" />
-      </div>
+      <img
+        src="https://raw.githubusercontent.com/Pancartex/meme-generator/062a595227cad524104beccffc8b6401c7606737/src/images/troll-face.png"
+        className="header--image"
+      />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
+      <h4 className="header--project">React project built with 🖤</h4>
     </header>
   );
 }
